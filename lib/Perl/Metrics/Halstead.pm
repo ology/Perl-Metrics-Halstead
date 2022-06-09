e4e3387f69215a0d692f534462426b989ec2dd98
Gene Boggs <gene@ology.net>
2022-06-08 17:31:57 -0700

    Remove debugging code

--- lib/Perl/Metrics/Halstead.pm
+++ lib/Perl/Metrics/Halstead.pm
@@ -262,7 +262,6 @@ sub BUILD {
         next if $item[0] eq 'PPI::Token::Pod' or $item[0] eq 'PPI::Token::End';
         push @{ $halstead{ $item[0] } }, $item[1];
     }
-#use Data::Dumper;warn(__PACKAGE__,' ',__LINE__,' ',Dumper\%halstead);
 
     $self->{n_operators} = 0;
     $self->{n_operands}  = 0;
@@ -288,7 +287,6 @@ sub BUILD {
             }
         }
     }
-#use Data::Dumper;warn(__PACKAGE__,' ',__LINE__,' ',Dumper\%distinct);
 
     $self->{n_distinct_operators} = keys %{ $distinct{operators} };
     $self->{n_distinct_operands}  = keys %{ $distinct{operands} };
